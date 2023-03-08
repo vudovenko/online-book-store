ea7654017f62351a96a4072ef15ff174f7a54bbb
vudovenko <vladislav.udovenko0@yandex.ru>
2023-03-08 22:56:44 +0500

    Replaced JdbcTemplate with JpaRepository

--- src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -1,50 +1,22 @@
 package com.example.MyBookShopApp.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Service
 public class BookService {
 
-    private final JdbcTemplate jdbcTemplate;
-    private final AuthorService authorService;
+    private final BookRepository bookRepository;
 
-    Logger logger = Logger.getLogger(AuthorService.class.getName());
 
     @Autowired
-    public BookService(JdbcTemplate jdbcTemplate, AuthorService authorService) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.authorService = authorService;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     public List<Book> getBooksData() {
-        List<Book> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rowNum) -> {
-            Book book = new Book();
-            book.setId(rs.getInt("id"));
-            book.setAuthor(getAuthorByAuthorId(rs.getInt("author_id")));
-            book.setTitle(rs.getString("title"));
-            book.setPriceOld(rs.getInt("price_old"));
-            book.setPrice(rs.getInt("price"));
-            return book;
-        });
-        return new ArrayList<>(books);
-    }
-
-    private String getAuthorByAuthorId(int authorId) {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors WHERE authors.id=" + authorId,
-                (ResultSet rs, int rowNum) -> {
-                    Author author = new Author();
-                    author.setId(rs.getInt("id"));
-                    author.setFirstName(rs.getString("first_name"));
-                    author.setLastName(rs.getString("last_name"));
-                    return author;
-                });
-        return authors.get(0).toString();
+        return bookRepository.findAll();
     }
 }
