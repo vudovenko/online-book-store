22a4b30ea30c86577df65c48a9e2d84334a623ab
vudovenko <vladislav.udovenko0@yandex.ru>
2023-10-17 00:19:38 +0500

    the 'Popular' page has been implemented. Pagination has been made for the output of books

--- src/main/java/com/example/MyBookShopApp/controllers/PopularPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/PopularPageController.java
@@ -2,6 +2,7 @@ package com.example.MyBookShopApp.controllers;
 
 import com.example.MyBookShopApp.data.book.entities.Book;
 import com.example.MyBookShopApp.data.book.services.BookService;
+import com.example.MyBookShopApp.data.book.services.BooksRatingAndPopularityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,17 @@ import java.util.List;
 @RequestMapping("/books")
 public class PopularPageController {
 
-    private final BookService bookService;
+    private final BooksRatingAndPopularityService booksRatingAndPopularityService;
 
     @Autowired
-    public PopularPageController(BookService bookService) {
-        this.bookService = bookService;
+    public PopularPageController(BookService bookService,
+                                 BooksRatingAndPopularityService booksRatingAndPopularityService) {
+        this.booksRatingAndPopularityService = booksRatingAndPopularityService;
     }
 
     @ModelAttribute("popularBooks")
-    public List<Book> recentBooks() {
-        return bookService.getBooksData();
+    public List<Book> popularBooks() {
+        return booksRatingAndPopularityService.getPopularBooks(0, 20).getContent();
     }
 
     @GetMapping("/popular")
